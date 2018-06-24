04d869997ac4088e97111e14cf137a57ac7cf8a0
danielblokus <daniel.blokus@gmail.com>
2018-06-24 17:56:53 +0200

    Create base class for test classes

---
+++ src/test/java/exercises/BaseApiTest.java
@@ -0,0 +1,19 @@
+package exercises;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+class BaseApiTest {
+
+    private static final String HOST_URL = "http://localhost";
+    private static final int PORT = 9876;
+    private static final String BASE_PATH = "/api/f1";
+
+    RequestSpecification createRequestSpecification() {
+        return new RequestSpecBuilder().
+                setBaseUri(HOST_URL).
+                setPort(PORT).
+                setBasePath(BASE_PATH).
+                build();
+    }
+}

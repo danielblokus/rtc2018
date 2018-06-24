e82608e72164399389ba2966ff56aa60d2de675a
danielblokus <daniel.blokus@gmail.com>
2018-06-24 18:05:59 +0200

    Clean imports

--- src/test/java/exercises/RestAssuredExercises2ApiTest.java
+++ src/test/java/exercises/RestAssuredExercises2ApiTest.java
@@ -1,17 +1,10 @@
 package exercises;
 
-import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(DataProviderRunner.class)
 public class RestAssuredExercises2ApiTest extends BaseApiTest {
